552754e3d20c12100669b59d91c2e8e742ed74ac
kfareg <k.fareg@optum.com>
2019-02-24 19:16:14 -0500

    first commit

---
+++ StMaryStBishoyChurch/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  StMaryStBishoyChurch
+//
+//  Created by Fareg, Kerules on 2/24/19.
+//  Copyright © 2019 Fareg, Kerules. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ StMaryStBishoyChurch/Multimedia/MediaViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  StMaryStBishoyChurch
+//
+//  Created by Fareg, Kerules on 2/24/19.
+//  Copyright © 2019 Fareg, Kerules. All rights reserved.
+//
+
+import UIKit
+
+class MediaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
