c12009852b049700acc0bc6b3ad2c7afbf7f3ce8
Kerules Fareg <Kerullos60@gmail.com>
2019-04-25 15:59:15 -0400

    Added Notification Alert to enable active Notifications

--- StMaryStBishoyChurch/AppDelegate.swift
+++ StMaryStBishoyChurch/AppDelegate.swift
@@ -122,7 +122,7 @@ extension AppDelegate : UNUserNotificationCenterDelegate {
         print(userInfo)
         
         // Change this to your preferred presentation option
-        completionHandler([])
+        completionHandler([.alert])
     }
     
     func userNotificationCenter(_ center: UNUserNotificationCenter,
